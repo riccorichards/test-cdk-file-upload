503e17f34843f480e3765a0152912484d128be7c
Riccorichards <richard.tropimovi.1@iliauni.edu.ge>
2024-04-23 18:11:39 +0400

    test commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { getSignUrl, uploadFileUsingPresignedUrl } from "./helper";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [file, setFile] = useState<string | null>(null);
+
+  const submitFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      try {
+        const signUrl = await getSignUrl(file);
+        if (signUrl) {
+          const uploadedFile = await uploadFileUsingPresignedUrl(file, signUrl);
+          setFile(uploadedFile);
+        }
+      } catch (error) {
+        console.error("Error uploading file:", error);
+        setFile(null);
+      }
+    }
+  };
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <input type="file" onChange={submitFile} />
+        {file && <img src={file} alt="" />}
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/helper.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const getSignUrl = async (file: File) => {
+  try {
+    const response = await axios.post(
+      `https://heu9f7ek9l.execute-api.eu-north-1.amazonaws.com/prod/upload?name=${file.name}&type=${file.type}`
+    );
+    const { url } = response.data;
+
+    const result = await axios.put(url, file, {
+      headers: {
+        "Content-Type": file.type,
+      },
+    });
+
+    return result.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.message);
+    }
+    throw error;
+  }
+};
+
+export const uploadFileUsingPresignedUrl = async (
+  file: File,
+  presignedUrl: string
+) => {
+  try {
+    const { data } = await axios.put(presignedUrl, file, {
+      headers: {
+        "Content-Type": file.type,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
